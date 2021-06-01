1b672ca261fd8c4e3eaadc7ce50ddbb4df27efa4
Panikat <panichuck.kat@gmail.com>
2021-06-01 21:06:34 +0300

    Add Taglib part

--- SS_TeamProject/SS_TeamProject.cpp
+++ SS_TeamProject/SS_TeamProject.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <string> 
 #include <vector>
-
+#include <taglib/fileref.h>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 void ShowAllFiles(string path_str);
 vector<const char*> FindMP3(string path_str);
+void PrintTags(vector<TagLib::FileRef> files, int num);
 
 int main()
 {
@@ -19,8 +20,12 @@ int main()
 	getline(cin, path_str);
 
 	//ShowAllFiles(path_str);
-	FindMP3(path_str);
-
+	vector<const char*> paths(FindMP3(path_str));
+	vector<TagLib::FileRef> files(paths.size());
+	for (int i=0; i < files.size(); ++i)
+		files[i].create(paths[i]);
+	for (int i = 0; i < files.size(); ++i)
+		PrintTags(files, files.size());
 }
 
 //Âûגמהטע גסו פאיכû
@@ -80,4 +85,15 @@ vector<const char*> FindMP3(string path_str)
 	}
 
 	return paths;
+}
+
+void PrintTags(vector<TagLib::FileRef> files, int num) {
+	cout << "______________________________________________" << endl;
+	cout << "Title: \t\t" << files[num].tag()->title() << endl;
+	cout << "Artist: \t\t" << files[num].tag()->artist() << endl;
+	cout << "Album: \t" << files[num].tag()->album() << endl;
+	cout << "Year:" << files[num].tag()->year() << endl;
+	cout << "Track:" << files[num].tag()->track() << endl;
+	cout << "Comment: \t" << files[num].tag()->comment() << endl;
+	cout << "Genre: \t\t" << files[num].tag()->genre() << endl;
 }
